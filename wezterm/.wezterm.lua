3e12fbd9cbb9ad6dd02e712129b78859187aca38
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-11-17 08:32:19 +0530

    Set max fps to 120

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,5 +7,6 @@ config.font_size = 13.0
 config.adjust_window_size_when_changing_font_size = false
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
+config.max_fps = 120
 
 return config
