9ea252bc36fb06d537da954788d1078cd2410229
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-03 01:39:16 +0530

    Add few fzf configurations

--- tools/fish.nix
+++ tools/fish.nix
@@ -12,7 +12,12 @@
     ];
     interactiveShellInit = ''
       set fish_greeting
-      set -gx BAT_THEME "Catppuccin Mocha"
+      set -Ux BAT_THEME "Catppuccin Mocha"
+      set -Ux FZF_DEFAULT_COMMAND "fd --hidden --strip-cwd-prefix --exclude .git"
+      set -Ux FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+      set -Ux FZF_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+      set -Ux FZF_CTRL_T_OPTS "--preview 'bat --color=always -n --line-range :500 {}'"
+      set -Ux FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
       set -Ux FZF_DEFAULT_OPTS "\
         --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
         --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
