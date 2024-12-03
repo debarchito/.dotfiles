ca0f8fe336d52834fffa2ce55f1723508470345b
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-03 17:42:41 +0530

    Format changes

--- flake.nix
+++ flake.nix
@@ -31,9 +31,7 @@
         };
       };
       systemConfigs.default = system-manager.lib.makeSystemConfig {
-        modules = [
-          ./modules
-        ];
+        modules = [ ./modules ];
       };
     };
 }
