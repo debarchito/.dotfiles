281f73bdf3188ae45f37d7cfed5c0ee1029391a2
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-03 23:25:11 +0530

    Configure formatters among other things

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
-  description = "Base flake";
+  description = "Root flake for my Pop!_OS setup";
   inputs = {
+    flake-root.url = "github:srid/flake-root";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,26 +18,43 @@
     };
   };
   outputs =
-    {
-      nixpkgs,
-      home-manager,
-      system-manager,
-      ...
-    }:
+    inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import inputs.nixpkgs { inherit system; };
     in
-    {
-      formatter.${system} = pkgs.nixfmt-rfc-style;
-      homeConfigurations = {
-        debarchito = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [ system ];
+      flake = {
+        homeConfigurations = {
+          debarchito = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+          };
+        };
+        systemConfigs.default = inputs.system-manager.lib.makeSystemConfig {
+          modules = [ ./modules ];
         };
       };
-      systemConfigs.default = system-manager.lib.makeSystemConfig {
-        modules = [ ./modules ];
-      };
+      perSystem =
+        { config, ... }:
+        {
+          # Configure all the formatters in one place
+          treefmt.config = {
+            inherit (config.flake-root) projectRootFile;
+            programs.just.enable = true;
+            programs.deno.enable = true;
+            programs.nixfmt.enable = true;
+            programs.stylua.enable = true;
+            programs.yamlfmt.enable = true;
+            programs.jsonfmt.enable = true;
+            programs.toml-sort.enable = true;
+            programs.fish_indent.enable = true;
+          };
+        };
     };
 }
