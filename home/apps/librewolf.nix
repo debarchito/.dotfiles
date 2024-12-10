20a19a648e77fc3075e528dc449c828298567fbe
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-10 16:22:38 +0530

    Basic Librewolf config

--- home/apps/librewolf.nix
+++ home/apps/librewolf.nix
@@ -4,9 +4,28 @@
   programs.librewolf = {
     enable = true;
     profiles.debarchito = {
+      search.engines = {
+        "Nix Packages" = {
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              { name = "type"; value = "packages"; }
+              { name = "query"; value = "{searchTerms}"; }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          }];
+        };
+      };
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
         darkreader
+        firefox-color
       ];
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+      userChrome = builtins.readFile ./librewolf/userChrome.css;
     };
   };
 }
