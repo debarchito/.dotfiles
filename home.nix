a42270d9022c35139c605cea531801e39a80f7e6
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-27 09:19:44 +0530

    Add yazi and move the session variables from fish to home.sessionVariables

--- home.nix
+++ home.nix
@@ -36,6 +36,7 @@
       nil
       ripgrep
       ripgrep-all
+      yazi
       zellij
       # languages
       jdk23
@@ -56,6 +57,22 @@
       (config.lib.nixGL.wrap blender)
       (config.lib.nixGL.wrap krita)
     ]);
+  home.sessionVariables = rec {
+    EDITOR = "hx";
+    BAT_THEME = "Catppuccin Mocha";
+    FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
+    FZF_CTRL_T_COMMAND = FZF_DEFAULT_COMMAND;
+    FZF_ALT_C_COMMAND = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+    FZF_CTRL_T_OPTS = "--preview 'bat --color=always -n --line-range :500 {}'";
+    FZF_ALT_C_OPTS = "--preview 'eza --tree --color=always {} | head -200'";
+    FZF_DEFAULT_OPTS = ''
+      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+      --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+      --color=selected-bg:#45475a \
+      --multi
+    '';
+  };
   programs.home-manager.enable = true;
   imports = [
     ./home/tools/cargo.nix
