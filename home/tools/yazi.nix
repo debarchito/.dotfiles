2243aeb7c58bf70bd27bbf250635c13ae47cfa30
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-28 13:56:12 +0530

    Set yazi theme

--- home/tools/yazi.nix
+++ home/tools/yazi.nix
@@ -4,4 +4,6 @@
     enableFishIntegration = true;
     enableNushellIntegration = true;
   };
+  xdg.configFile."yazi/theme.toml".source = ./yazi/theme.toml;
+  xdg.configFile."yazi/Catppuccin-mocha.tmTheme".source = ./yazi/Catppuccin-mocha.tmTheme;
 }
