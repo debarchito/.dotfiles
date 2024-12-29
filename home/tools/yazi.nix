5ef328d1e9c7a685d47ecc7a1001a1342797de7a
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-12-29 18:15:02 +0530

    Update yazi ratio

--- home/tools/yazi.nix
+++ home/tools/yazi.nix
@@ -4,6 +4,7 @@
     enableFishIntegration = true;
     enableNushellIntegration = true;
   };
+  xdg.configFile."yazi/yazi.toml".source = ./yazi/yazi.toml;
   xdg.configFile."yazi/theme.toml".source = ./yazi/theme.toml;
   xdg.configFile."yazi/Catppuccin-mocha.tmTheme".source = ./yazi/Catppuccin-mocha.tmTheme;
 }
