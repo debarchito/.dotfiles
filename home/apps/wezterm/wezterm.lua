89bcb6f0953c565fbd847cdb78dab2599fa3fd1c
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-01-02 10:17:16 +0530

    Tab bar modifications to WezTerm

--- home/apps/wezterm/wezterm.lua
+++ home/apps/wezterm/wezterm.lua
@@ -4,11 +4,13 @@ local config = wezterm.config_builder()
 config.max_fps = 120
 config.font_size = 13.0
 config.front_end = "WebGpu"
-config.enable_tab_bar = false
 config.default_prog = { "fish" }
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
 config.window_close_confirmation = "NeverPrompt"
 config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 return config
