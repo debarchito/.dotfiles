97e86a8e0dc6d98d555d48a10b3a6bb137ff7f0c
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-01-19 23:22:41 +0530

    Remove zellij integeration with fish

--- home/tools/zellij.nix
+++ home/tools/zellij.nix
@@ -1,7 +1,6 @@
 {
   programs.zellij = {
     enable = true;
-    enableFishIntegration = true;
   };
   xdg.configFile."zellij/config.kdl".source = ./zellij/config.kdl;
   xdg.configFile."zellij/themes/catppuccin.kdl".source = ./zellij/themes/catppuccin.kdl;
