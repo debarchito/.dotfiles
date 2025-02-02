8506af990c78dbaaa1ee62afa59df5ac07868520
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-02-02 18:11:42 +0530

    Fix bat theme

--- home/tools/bat.nix
+++ home/tools/bat.nix
@@ -2,7 +2,8 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "Catppuccin Mocha";
+      theme = "catppuccin_mocha";
     };
   };
+  xdg.configFile."bat/themes/catppuccin_mocha.tmTheme".source = ./bat/catppuccin_mocha.tmTheme;
 }
