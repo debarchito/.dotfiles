700ab2cb84cd3e24a4fd6730e71d28955ba3c7f2
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-07-09 17:47:23 +0530

    Remove scripts

--- scripts/setup-plasma.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-# Setup Plasma the way I like it
-lookandfeeltool -a Catppuccin-Mocha-Mauve
-kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key library --type string "org.kde.breeze"
-kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key theme --type string Breeze
-kwriteconfig6 --file kcminputrc --group Mouse --key cursorTheme --type string Bibata-Modern-Ice
-kwriteconfig6 --file kcminputrc --group Mouse --key cursorSize --type int 24
-kwin_wayland --replace &
-plasmashell --replace &
--- scripts/setup-waydroid.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-# Setup waydroid things
-sudo waydroid init -s GAPPS -c https://ota.waydro.id/system -v https://ota.waydro.id/vendor -f
-sudo waydroid-script install libhoudini
-echo -e "\nro.hardware.gralloc=default\nro.hardware.egl=swiftshader" | sudo tee -a /var/lib/waydroid/waydroid.cfg
-sudo waydroid upgrade -o
