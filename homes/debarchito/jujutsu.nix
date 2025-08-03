9068651d1319c4802716425a9dd52269e256c1d3
Debarchito Nath <debarchiton@proton.me>
2025-08-03 08:26:22 +0530

    Let JJ ignore Git LFS files

--- homes/debarchito/jujutsu.nix
+++ homes/debarchito/jujutsu.nix
@@ -3,7 +3,6 @@
     enable = true;
     ediff = true;
     settings = {
-      ui.default-command = "log";
       user = {
         name = "Debarchito Nath";
         email = "debarchiton@proton.me";
@@ -13,6 +12,8 @@
         backend = "gpg";
         key = "A16B1370EE2B0F04";
       };
+      ui.default-command = "log";
+      git.ignore-files = [ "lfs" ];
     };
   };
 }
