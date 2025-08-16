06ca0477cad674d53ff91422b4eeb6973005e9bd
Debarchito Nath <debarchiton@proton.me>
2025-08-16 19:16:20 +0530

    more apps in plasma6 excluded list

--- modules/wm/plasma.nix
+++ modules/wm/plasma.nix
@@ -3,6 +3,8 @@
 {
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = [
+    pkgs.kdePackages.elisa
     pkgs.kdePackages.kate
+    pkgs.kdePackages.khelpcenter
   ];
 }
