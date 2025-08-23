c1afdf5fc2322da24396966a87a174f573b62b7a
Debarchito Nath <debarchiton@proton.me>
2025-08-23 18:17:32 +0530

    update atuin config

--- homes/debarchito/atuin.nix
+++ homes/debarchito/atuin.nix
@@ -2,5 +2,10 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      style = "full";
+      show_help = false;
+      show_tabs = false;
+    };
   };
 }
