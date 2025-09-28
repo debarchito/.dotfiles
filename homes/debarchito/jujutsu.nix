f8da5bda9bcbf76bacbe71126e96d5b52ef71e1b
Debarchito Nath <debarchiton@proton.me>
2025-09-28 14:11:24 +0530

    feat(jujutsu): enable the pager after all
    reading diffs is a curse without the default pager

--- homes/debarchito/jujutsu.nix
+++ homes/debarchito/jujutsu.nix
@@ -12,10 +12,7 @@
         backend = "gpg";
         key = "5D40AFCF09EE4500";
       };
-      ui = {
-        paginate = "never";
-        default-command = "log";
-      };
+      ui.default-command = "log";
       git.ignore-files = [ "lfs" ];
     };
   };
