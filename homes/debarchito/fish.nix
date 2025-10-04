9e94c0722919f64e5d8e3aa84e0c3a116695c494
Debarchito Nath <debarchiton@proton.me>
2025-10-04 11:53:31 +0530

    feat(curl): add a conv curl abbr to fish

--- homes/debarchito/fish.nix
+++ homes/debarchito/fish.nix
@@ -74,24 +74,9 @@
         setCursor = "%";
         expansion = "jj log -r 'heads(all())' -n % --no-pager";
       };
-      gh_unghost = {
+      co = {
         setCursor = "%";
-        expansion = ''
-          curl -L \
-            -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer $(gh auth token)" \
-            -H "X-GitHub-Api-Version: 2022-11-28" \
-            'https://api.github.com/notifications?all=true&since=2025-09-%T00:00:00Z' > ~/.config/default/gh-unghost.json
-          for THREAD_URL in (jq -r '.[].url' ~/.config/default/gh-unghost.json)
-            curl -L \
-               -X DELETE \
-               -H "Accept: application/vnd.github+json" \
-               -H "Authorization: Bearer $(gh auth token)" \
-               -H "X-GitHub-Api-Version: 2022-11-28" \
-               "$THREAD_URL"
-          end
-          rm ~/.config/default/gh-unghost.json
-        '';
+        expansion = "curl -sIL % | rg location:";
       };
     };
     functions = {
