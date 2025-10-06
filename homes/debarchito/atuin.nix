000d7ea72269b8e7d3d50cfe253945fa1c698977
Debarchito Nath <debarchiton@proton.me>
2025-10-06 09:13:21 +0530

    fix(atuin): disable up arrow key for atuin

--- homes/debarchito/atuin.nix
+++ homes/debarchito/atuin.nix
@@ -2,6 +2,9 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    flags = [
+      "--disable-up-arrow"
+    ];
     settings = {
       style = "full";
       show_help = false;
