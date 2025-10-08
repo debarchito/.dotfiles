a6f7fed2a4bf3e3e7ef61fd90b1aa34ee56b965f
Debarchito Nath <debarchiton@proton.me>
2025-10-08 09:33:18 +0530

    fix(qemu): qemu.ovfm is not required in vm.nix

--- modules/hosts/vm.nix
+++ modules/hosts/vm.nix
@@ -20,15 +20,6 @@
         qemu = {
           runAsRoot = true;
           swtpm.enable = true;
-          ovmf = {
-            enable = true;
-            packages = [
-              (pkgs.OVMF.override {
-                secureBoot = true;
-                tpmSupport = true;
-              }).fd
-            ];
-          };
           vhostUserPackages = [ pkgs.virtiofsd ];
         };
       };
