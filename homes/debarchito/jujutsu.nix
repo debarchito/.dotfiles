674507bd266af0b2ce46c4bc779f541a92726024
Debarchito Nath <debarchiton@proton.me>
2025-10-08 09:33:18 +0530

    feat(jujutsu): add jjui

--- homes/debarchito/jujutsu.nix
+++ homes/debarchito/jujutsu.nix
@@ -1,25 +1,27 @@
 {
-  programs.jujutsu = {
-    enable = true;
-    ediff = true;
-    settings = {
-      user = {
-        name = "Debarchito Nath";
-        email = "debarchiton@proton.me";
-      };
-      signing = {
-        behavior = "own";
-        backend = "gpg";
-        key = "5D40AFCF09EE4500";
-      };
-      ui = {
-        default-command = "log";
-        diff-editor = ":builtin";
-      };
-      git = {
-        colocate = true;
-        ignore-files = [ "lfs" ];
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Debarchito Nath";
+          email = "debarchiton@proton.me";
+        };
+        signing = {
+          behavior = "own";
+          backend = "gpg";
+          key = "5D40AFCF09EE4500";
+        };
+        ui = {
+          default-command = "log";
+          diff-editor = ":builtin";
+        };
+        git = {
+          colocate = true;
+          ignore-files = [ "lfs" ];
+        };
       };
     };
+    jjui.enable = true;
   };
 }
