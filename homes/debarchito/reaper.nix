847d28f515c888234440342adf46238b73b8d9fb
Debarchito Nath <debarchiton@proton.me>
2025-12-18 23:40:49 +0530

    feat(yabridge): add yabridge

--- homes/debarchito/reaper.nix
+++ homes/debarchito/reaper.nix
@@ -3,6 +3,8 @@
 {
   home.packages = [
     pkgs.reaper
+    pkgs.yabridge
+    pkgs.yabridgectl
   ];
   home.file.".lv2/neural_amp_modeler.lv2".source =
     "${pkgs.neural-amp-modeler-lv2}/lib/lv2/neural_amp_modeler.lv2";
