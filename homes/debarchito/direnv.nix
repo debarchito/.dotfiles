ff40e67d72d017225f9a583865ad2e3ed3103e22
Debarchito Nath <debarchiton@proton.me>
2026-01-24 15:53:31 +0530

    chore(direnv): make direnv less verbose

--- homes/debarchito/direnv.nix
+++ homes/debarchito/direnv.nix
@@ -2,4 +2,5 @@
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
   programs.direnv.enableNushellIntegration = true;
+  programs.direnv.config.global.hide_env_diff = true;
 }
