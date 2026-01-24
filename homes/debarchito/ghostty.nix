b54f1bc14b3571d97e1a55b22ba79aab7fac223e
Debarchito Nath <debarchiton@proton.me>
2026-01-24 14:32:23 +0530

    fix(ghostty): adjust font-features

--- homes/debarchito/ghostty.nix
+++ homes/debarchito/ghostty.nix
@@ -8,7 +8,7 @@
     window-padding-y = "0,0";
     font-family = "Maple Mono NF";
     font-size = 14;
-    font-feature = "-calt, -liga, -dlig";
+    font-feature = "-calt,-zero,-cv02,+cv01,+cv61";
     config-file = "./themes/dankcolors";
     app-notifications = "no-clipboard-copy,no-config-reload";
   };
