0dfdff50a51324a73f46d098ef1dd59cb7ec7e12
Debarchito Nath <debarchiton@proton.me>
2026-01-24 16:22:25 +0530

    feat(zellij): make zellij theme matugen native

--- homes/debarchito/ghostty.nix
+++ homes/debarchito/ghostty.nix
@@ -7,7 +7,7 @@
     window-padding-x = 10;
     window-padding-y = "0,0";
     font-family = "Maple Mono NF";
-    font-size = 14;
+    font-size = 15;
     font-feature = "-calt,-zero,-cv02,+cv01,+cv61";
     config-file = "./themes/dankcolors";
     app-notifications = "no-clipboard-copy,no-config-reload";
--- homes/debarchito/zellij.nix
+++ homes/debarchito/zellij.nix
@@ -2,6 +2,5 @@
   programs.zellij.enable = true;
   programs.zellij.enableFishIntegration = false;
 
-  xdg.configFile."zellij/themes".source = ./zellij/themes;
   xdg.configFile."zellij/config.kdl".source = ./zellij/config.kdl;
 }
