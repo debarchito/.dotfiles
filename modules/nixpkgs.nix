d349b12baf1df44ce2d4b7b471f8c407a687c354
Debarchito Nath <debarchiton@proton.me>
2026-02-27 16:57:32 +0530

    feat(lock): update lock file

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -2,6 +2,7 @@
 {
   flake-file.inputs = {
     nixpkgs.url = lib.mkForce "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs-1.url = lib.mkDefault "github:nixos/nixpkgs/7d1e6f1288a41e5fdbd4c5c82d495b51d49511a4";
     nixpkgs-lib.follows = lib.mkForce "nixpkgs";
   };
 
