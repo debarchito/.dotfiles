c0f9f0f05b7afd7dd22b0e57fbc49c6794b0249c
Debarchito Nath <debarchiton@proton.me>
2026-03-04 14:15:44 +0530

    feat(lock): update lock file

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    nixpkgs-1.url = "github:nixos/nixpkgs/7d1e6f1288a41e5fdbd4c5c82d495b51d49511a4";
+    nixpkgs-1.url = "github:nixos/nixpkgs/e4bf2a3807d74a0634fdc17de7622ce1be9c8cee";
     nixpkgs-lib.follows = "nixpkgs";
     nur = {
       inputs.nixpkgs.follows = "nixpkgs";
@@ -44,7 +44,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       url = "gitlab:lanastara_foss/starship-jj";
     };
-    systems.url = "github:nix-systems/default";
     treefmt-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
