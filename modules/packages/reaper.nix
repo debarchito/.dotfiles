bcdc5c59acdbc377c8f8f40072cd2a5825bc5998
Debarchito Nath <debarchiton@proton.me>
2026-03-06 21:01:51 +0530

    chore(packages): update reaper to 7.64

--- modules/packages/reaper.nix
+++ modules/packages/reaper.nix
@@ -8,18 +8,18 @@
     }:
     let
       version = rec {
-        value = "7.62";
+        value = "7.64";
         major = "${builtins.head (lib.splitString "." value)}.x";
         slug = lib.replaceString "." "" value;
       };
       sources = {
         "x86_64-linux" = {
           url = "https://www.reaper.fm/files/${version.major}/reaper${version.slug}_linux_x86_64.tar.xz";
-          hash = "sha256-BkU8/PJdJRtRRVV/HJBAIxzCgtYHjHtCGHxSPw08eDM=";
+          hash = "sha256-mw2m/lTMDbg1gS2g998eNbAo9xb3GZ4zIvMRRNDz85k=";
         };
         "aarch64-linux" = {
           url = "https://www.reaper.fm/files/${version.major}/reaper${version.slug}_linux_aarch64.tar.xz";
-          hash = "sha256-O8Oyjxeg7lOa3wPCc6O88iY0m2HJHcUwRBoUnUYVO+Q=";
+          hash = "sha256-C5pQfztoC3+HcQeNbG2Tbe63PTWfTGHvNOlIhiVgDp4=";
         };
       };
     in
