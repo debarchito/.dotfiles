220ac058671c72379949d0ebcde2fba7ac6352ad
Debarchito Nath <debarchiton@proton.me>
2026-03-08 12:20:26 +0530

    chore(refactor): small refactors

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,8 +1,8 @@
 { lib, inputs, ... }:
 {
-  flake-file.inputs = {
-    flake-parts.url = lib.mkDefault "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = lib.mkDefault "nixpkgs-lib";
+  flake-file.inputs.flake-parts = {
+    url = lib.mkDefault "github:hercules-ci/flake-parts";
+    inputs.nixpkgs-lib.follows = lib.mkDefault "nixpkgs-lib";
   };
 
   imports = [
