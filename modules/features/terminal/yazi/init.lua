30ad39fba478b2b135250daf87d3fb26f7624473
Debarchito Nath <debarchiton@proton.me>
2026-03-21 21:28:31 +0530

    feat(yazi): trash bin integration

--- modules/features/terminal/yazi/init.lua
+++ modules/features/terminal/yazi/init.lua
@@ -1,11 +1,13 @@
+require("duckdb"):setup()
+
 require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
-
-require("duckdb"):setup()
 
 require("git"):setup()
 
 require("no-header"):setup()
 
 require("no-status"):setup()
+
+require("recycle-bin"):setup()
