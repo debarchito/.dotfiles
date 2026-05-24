a8a20205f36369de51e5c93457404d4f865b2288
Debarchito Nath <debarchiton@proton.me>
2026-05-24 19:35:15 +0530

    feat(games): more gaming stuff

--- modules/features/trusted-substituters.nix
+++ modules/features/trusted-substituters.nix
@@ -15,12 +15,14 @@
         nix.settings.substituters = [
           "https://attic.xuyh0120.win/lantian"
           "https://debarchito.cachix.org"
+          "https://ezkea.cachix.org"
           "https://install.determinate.systems"
           "https://nix-community.cachix.org"
         ];
         nix.settings.trusted-public-keys = [
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "debarchito.cachix.org-1:md/bk3JZDoFjVOa6bsIDqaY5hcSec4KPWn8q3PbpCl8="
+          "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
           "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
